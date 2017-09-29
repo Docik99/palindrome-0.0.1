ec9f228c5a904ec2daa5618a89e68f1156088c20
Docik99 <32065929+Docik99@users.noreply.github.com>
2017-09-29 22:01:40 +0300

    Add files via upload

---
+++ palindrome-0.0.1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int len, k, r;
+    string str;
+
+    cin >> str;
+    len = str.length();
+    for (int i = 0; i <= len / 2 - 1; i++)
+    {
+        if (str[i] == str[len - i - 1])
+        {
+            k = k + 1;
+        }
+    }
+
+    if (k == len / 2)
+    {
+        cout << "1";
+    }
+    else
+    {
+        cout << "0";
+    }
+    return 0;
+}
